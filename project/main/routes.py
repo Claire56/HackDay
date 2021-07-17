c3a1143abe25fa061444b62af48510115766d239
claire muwanguzi <kclaire56@yahoo.com>
2021-07-16 18:32:04 -0700

    day two

--- project/main/routes.py
+++ project/main/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, url_for, redirect, request, Blueprint
+from flask_login import current_user, login_required
 from project.models import Bplan
 
 main = Blueprint('main', __name__)
@@ -7,10 +8,37 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    
-    return render_template('home.html')
+    if current_user.is_authenticated:
+        return render_template('home.html')
+    return render_template('login.html')
 
 
 @main.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@main.route("/business_plan", methods=['GET', 'POST'])
+@login_required
+def business_plan():
+    if request.method == "POST":
+        data = request.form
+        new_plan = Bplan(
+                        title = data['title'],
+                        industry = data['industry'],
+                        funds_needed = data['funds_needed'],
+                        content = data['content'],
+                        user_id = current_user.id,
+                    )
+        new_plan.save()
+        return redirect('/home')
+    else:
+        return render_template('create_bplan.html')
+
+
+@main.route("/my_plans")
+@login_required
+def show_business_plans():
+    bplans = Bplan.query.filter_by(user_id=current_user.id)
+    return render_template('bplans.html',bplans=bplans)
+    
--- readme.md
+++ readme.md
@@ -1,12 +1,63 @@
-create a virtualenv
+## How to Run Project
+### create a virtualenv
 $virtualenv env
-activate virtualenv
+### activate virtualenv
 $source env/bin/activate
-install packages 
+## install packages 
 $pip3 install -r requirements.txt
-create database 
+### create database 
 $python
->>> from project import db
+>>> from project import db <br>
 >>> db.create_all()
-run app
+### run app
 $python server.py
+
+
+# Main Screen Flow
+
+## Structure
+Defines the business structure
+
+![Structure](./docs/1.Structure.png)
+
+## Products for the 
+Defines the products the business is producing or is planning to produce
+![Products](./docs/2.Products.png)
+
+
+## Marketing
+Target and established markets. Advertising strategy and others
+![Marketing](./docs/3.Marketing.png)
+
+
+## SWOT
+SWOT analysis
+![SWOT](./docs/4.SWOT.png)
+
+
+## Operational
+Day-to-day ops, facilities and staffing
+
+![Operational](./docs/5.Operational.png)
+
+
+## Financial Details
+Defines the business structure
+
+![FinancialDetails](./docs/6.FinancialDetails.png)
+
+### Financial Statments
+Includes financial statments for cashflow forecasting
+
+![FinancialStatments](./docs/7.FinancialStatements.png)
+
+
+## Review Business
+Listing selectable list of business plans
+
+![ReviewBusinessCases](./docs/8.ReviewBusinessCases.png)
+
+## Data Model
+
+
+![bizPlanDataModel](./docs/bizPlanDataModel.png)
